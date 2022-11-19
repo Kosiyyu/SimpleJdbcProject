7842252eb41df204c70156a911d6f74fdee4dd8f
Kosiyyu <kosiyyu@gmail.com>
2022-11-19 23:51:26 +0100

    Updating database constants

--- src/main/java/org/example/database/Constants.java
+++ src/main/java/org/example/database/Constants.java
@@ -1,4 +1,17 @@
 package org.example.database;
 
 public class Constants {
+
+    //DATABASE
+    public static final String DATABASE_NAME = "test";
+    public static final String DATABASE_OWNER_LOGIN = "postgres";//(DO NOT MISTAKE WITH MODEL)
+    public static final String DATABASE_OWNER_PASSWORD = "alamakota123";//(DO NOT MISTAKE WITH MODEL)
+    public static final String DATABASE_SSL = "false";
+
+    //OWNER
+    public static final String OWNER_ID = "OWNER_ID";
+    public static final String OWNER_FIRSTNAME = "OWNER_FIRSTNAME";
+    public static final String OWNER_LASTNAME = "OWNER_LASTNAME";
+    public static final String OWNER_EMAIL = "OWNER_EMAIL";
+
 }
