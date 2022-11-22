5d7f4d098fa9e4c585e36576a75f6f7bc7b99099
Kosiyyu <kosiyyu@gmail.com>
2022-11-22 08:44:25 +0100

    Adding ui

--- src/main/java/org/example/view/MainView.java
+++ src/main/java/org/example/view/MainView.java
@@ -1,4 +1,23 @@
 package org.example.view;
 
+import javax.swing.*;
+
 public class MainView {
+    private JPanel rootPanel;
+    private JTextField firstnameTextField;
+    private JTextField lastnameTextField;
+    private JTextField emailTextField;
+    private JTable ownerTable;
+    private JButton addButton;
+    private JButton readButton;
+    private JButton updateButton;
+    private JButton deleteButton;
+    private JComboBox sortComboBox;
+    private JLabel firstnameJLabel;
+    private JLabel lastnameJLabel;
+    private JLabel emailJLabel;
+    private JPanel labelPanel;
+    private JPanel inputPanel;
+    private JPanel tablePanel;
+    private JPanel buttonPanel;
 }
